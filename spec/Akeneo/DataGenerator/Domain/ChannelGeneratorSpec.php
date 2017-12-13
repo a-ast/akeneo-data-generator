05ab6220318d8d54e552cf898fe0c72fb5faeaeb
Anael CHARDAN <anael.chardan@gmail.com>
2017-12-13 15:49:20 +0100

    :green_heart: Fix tests according to random

--- spec/Akeneo/DataGenerator/Domain/ChannelGeneratorSpec.php
+++ spec/Akeneo/DataGenerator/Domain/ChannelGeneratorSpec.php
@@ -41,16 +41,22 @@ class ChannelGeneratorSpec extends ObjectBehavior
         $currencyRepository,
         $categoryRepository,
         Locale $locale,
+        Locale $locale2,
         Currency $currency,
+        Currency $currency2,
         Category $tree
     ) {
-        $localeRepository->all()->willReturn([$locale]);
+        $localeRepository->all()->willReturn([$locale, $locale2]);
         $localeRepository->get('en_US')->willReturn($locale);
-        $currencyRepository->all()->willReturn([$currency]);
+        $localeRepository->get('fr_FR')->willReturn($locale2);
+        $currencyRepository->all()->willReturn([$currency, $currency2]);
         $currencyRepository->get('EUR')->willReturn($currency);
+        $currencyRepository->get('USD')->willReturn($currency2);
         $categoryRepository->allTrees()->willReturn([$tree]);
         $locale->code()->willReturn('en_US');
+        $locale2->code()->willReturn('fr_FR');
         $currency->code()->willReturn('EUR');
+        $currency2->code()->willReturn('USD');
 
         $this->generate(2, 2)->shouldBeAnInstanceOf(Channel::class);
     }
